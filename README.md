2110c6cd52098432db98ffe92029713580063bf3
tutorcs <tutorcs@163.com>
2023-03-06 12:04:01 +0800

    WeChat cstutorcs CSCI 520 Assignment 2 Motion Capture Interpolation

---
+++ README.md
@@ -0,0 +1,12 @@
+# CSCI 520 Assignment 2 Motion Capture Interpolation
+# WeChat: cstutorcs
+
+# QQ: 749389476
+
+# Email: tutorcs@163.com
+
+# Computer Science Tutor
+
+# Programming Help
+
+# Assignment Project Exam Help
---
+++ posture.cpp
@@ -0,0 +1,13 @@
+https://tutorcs.com
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+/*
+
+Revision 1 - Steve Lin, Jan. 14, 2002
+Revision 2 - Alla and Kiran, Jan 18, 2002
+Revision 3 - Jernej Barbic and Yili Zhao, Feb, 2012
+
+*/
+#include "posture.h"
+
